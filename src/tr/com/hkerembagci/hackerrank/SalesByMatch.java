5816577cb90833e85318c94f34f35e712f23e573
hkbagci <hkerembagci@gmail.com>
2022-01-25 22:44:21 +0300

    New Year Chaos and Sales By Match solutions.

---
+++ src/tr/com/hkerembagci/hackerrank/SalesByMatch.java
@@ -0,0 +1,58 @@
+package tr.com.hkerembagci.hackerrank;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+class SalesByMatchResult {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        int result = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < ar.size(); i++) {
+            map.merge(ar.get(i), 1, Integer::sum);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if((entry.getValue() / 2) > 0) {
+                result += entry.getValue() / 2;
+            }
+        }
+        return result;
+    }
+
+}
+
+public class SalesByMatch {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = SalesByMatchResult.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
